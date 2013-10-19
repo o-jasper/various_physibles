126613360c014bedea5ac7a69449978d7ae00b72
Jasper den Ouden <o.jasper@gmail.com>
2013-10-19 13:57:09 +0200

    Updated some shellscript.. really kindah needs these upto date

--- bin/escad.sh
+++ bin/escad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p `dirname $2`
+
 FILE=/tmp/`date +%s`$RANDOM.escad
 echo $1 > $FILE
 extopenscad $FILE -o $2
--- bin/scad.sh
+++ bin/scad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p `dirname $2`
+
 FILE=/tmp/`date +%s`$RANDOM.scad
 echo $1 > $FILE
 openscad $FILE -o $2
